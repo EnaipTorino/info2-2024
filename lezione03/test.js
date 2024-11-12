1c34c66ff075be2fbe9554dd4ee36d0c8355b8d0
Mattia Folcarelli <mattia.folcarelli@enaip.piemonte.it>
2024-11-12 13:27:25 +0100

    programma test

---
+++ lezione03/test.js
@@ -0,0 +1,24 @@
+let conteggioMare = 0
+let conteggioMontagna = 0
+let elemento = document.getElementById("risultato")
+
+let risposta = prompt("Ti piace il caldo o il freddo?")
+if (risposta == "caldo") {
+    conteggioMare = conteggioMare + 1
+} else {
+    conteggioMontagna = conteggioMontagna + 1
+}
+
+risposta = prompt("Ti piacciono le castagne")
+if (risposta == "si") {
+    conteggioMontagna = conteggioMontagna + 1
+} else {
+    conteggioMare = conteggioMare + 1
+}
+
+if( conteggioMare > conteggioMontagna){
+    elemento.innerText = "Sei un tipo da mare"
+}else{
+    elemento.innerText = "Sei un tipo da montagna"
+}
+console.log(conteggioMare, conteggioMontagna)
